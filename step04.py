29c800969750dae446a8c8c7b0c6a7141a9a74dc
Seoungjin Kim <capnaver@gmail.com>
2017-11-28 23:07:38 +0900

    다중 괄호 버전 완료

--- step04.py
+++ step04.py
@@ -12,11 +12,12 @@ def calculator(str):
         if item.isdigit():
             numbers.append(item)
         else:
-            y = int(numbers.pop())
-            x = int(numbers.pop())
+            y = float(numbers.pop())
+            x = float(numbers.pop())
+
             numbers.append(formula(item)(x, y))
 
-    return numbers[0]
+    return numbers[0], ''.join(get_postfix)
 
 
 # postfix후위 표기법으로 변환
@@ -27,33 +28,60 @@ def generator_postfix(str):
 
     for idx, item in enumerate(str.replace(' ', '')):
         if item.isdigit():
-            postfix_stack.append(item)
+            if ')' is not item:
+                postfix_stack.append(item)
 
         else:
             if len(oper_stack) == 0:
                 oper_stack.append(item)
-            
+
             elif oper_priority(item) > oper_priority(oper_stack[-1]):
                 if ')' is not item:
                     oper_stack.append(item)
 
-            elif oper_priority(item) < oper_priority(oper_stack[-1]):
-                if ')' is not item:
+            elif oper_priority(item) == oper_priority(oper_stack[-1]):
+                if '(' in oper_stack:
+                    if '(' is item:
+                        oper_stack.append(item)
+                    else:
+                        postfix_stack.append(oper_stack.pop())
+                else:
                     while oper_stack:
-                        value = oper_stack.pop()
-                        postfix_stack.append(value)
+                        postfix_stack.append(oper_stack.pop())
                     oper_stack.append(item)
 
-                if ')' is item:
-                    while oper_stack:
-                        value = oper_stack.pop()
-                        if '(' is value:
-                            break
-                        postfix_stack.append(value)
+            elif oper_priority(item) < oper_priority(oper_stack[-1]):
+                    if '(' in oper_stack:
+                        if ')' is not item:
+                            if '(' is item:
+                                oper_stack.append(item)
+                            else:
+                                while oper_stack:
+                                    value = oper_stack.pop()
+                                    if '(' is value:
+                                        oper_stack.append(item)
+                                        break
+                                    postfix_stack.append(value)
 
-            elif oper_priority(item) == oper_priority(oper_stack[-1]):
-                postfix_stack.append(oper_stack.pop())
-                oper_stack.append(item)
+                        if ')' is item:
+                            while oper_stack:
+                                value = oper_stack.pop()
+                                if '(' is not value:
+                                    postfix_stack.append(value)
+
+                                if value == '(':
+                                    break
+
+                    else:
+                        if '(' is item:
+                            oper_stack.append(item)
+                        else:
+                            while oper_stack:
+                                value = oper_stack.pop()
+                                postfix_stack.append(value)
+
+                            if ')' is not item:
+                                oper_stack.append(item)
 
     if '*' in oper_stack or '/' in oper_stack:
         while oper_stack:
@@ -65,8 +93,6 @@ def generator_postfix(str):
             value = oper_stack.pop()
             postfix_stack.append(value)
 
-    print(oper_stack)
-    print(postfix_stack)
     return postfix_stack
 
 
@@ -95,21 +121,25 @@ def formula(opr):
 
 if __name__ == '__main__':
     example = [
-        # '((4+2)/4) - (3+2/(7*5))',
-        # '3+2/(7*5)',
-        # '(1+2*3)/4',
-        # '(1*2+3)/4',
+        '((4+2)/4) - (3+2/(7*5))',
+        '3+2/(7*5)',
+        '(4+2)/4',
+        '(1+2*3)/4',
+        '(1*2+3)/4',
         '9-5-5*2-2*9',
-        # '1*2+3',
-        # '1+2*3',
-        # '1+1 + 1',
-        # '3-1 + 7',
-        # '3+1 - 7',
-        # '4- 1',
-        # '5 +1',
-        # '5 * 2-1',
-        # '8/ 2 +2',
+        '1*2+3',
+        '1+2*3',
+        '1+1 + 1',
+        '3-1 + 7',
+        '3+1 - 7',
+        '4- 1',
+        '5 +1',
+        '5 * 2-1',
+        '8/ 2 +2',
     ]
-    for e in example:
-        result = calculator(e)
-        print('calculation result : {} = {}({})'.format(e, int(result), result))
+    for idx, e in enumerate(example):
+        result, postfix = calculator(e)
+        int_tp = int(result)
+        float_tp = round(result, 5)
+        print('{}] postfix => {}'.format(idx, postfix))
+        print('calculation result : {} = {}({})\n'.format(e, int_tp, float_tp))
